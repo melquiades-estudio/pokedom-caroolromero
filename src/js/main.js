9e3ab0fc728e5ce127021b2e12f199ae15721211
unknown <carolf5laciba@gmail.com>
2022-05-31 14:24:14 +0200

    cambio foto nombre tipo

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,62 @@
 //import pokemons from "./data.js";
+
+let pokemons = document.querySelector (".list-pokemons");
+let nombrepokemon = [ "ivysaur","venusaur", "Charmander"]
+let tipopokemon = [ "water","fire", "gras"]
+let imagenpokemon = [ "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" , "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"]
+
+pokemons.innerHTML = ` 
+
+<article class="list-pokemons-item">
+          <div class="list-pokemons-item-content">
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+              alt="pokemon-1"
+            />
+            <h3>
+              Bulbasaur
+              <span>gras</span>
+            </h3>
+          </div>
+        </article>
+
+        <article class="list-pokemons-item">
+          <div class="list-pokemons-item-content">
+            <img
+              src= ${imagenpokemon[1]}
+              alt="pokemon-1"
+            />
+            <h3>
+            ${nombrepokemon[0]}
+              <span> ${tipopokemon[0]}</span>
+            </h3>
+          </div>
+        </article>
+
+        <article class="list-pokemons-item">
+          <div class="list-pokemons-item-content">
+            <img
+            src= ${imagenpokemon[0]}
+              alt="pokemon-1"
+            />
+            <h3>
+            ${nombrepokemon[1]}
+              <span> ${tipopokemon[0]}</span>
+            </h3>
+          </div>
+        </article>
+
+        <article class="list-pokemons-item">
+          <div class="list-pokemons-item-content">
+             <img
+              src= ${imagenpokemon[3]}
+              alt="pokemon-1"
+            />
+            <h3>
+            ${nombrepokemon[2]}
+              <span> ${tipopokemon[1]}</span>
+            </h3>
+          </div>
+        </article>
+
+        `
